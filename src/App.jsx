fac6b858fa6812215b5b0cf862675a3d3c2ae1ac
SItojm <sitojm.t@gmail.com>
2025-11-20 00:02:17 +0900

    2025年11月19日_Station05まで完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-// DO NOT DELETE
-
+import { useState } from "react";
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    "https://images.dog.ceo/breeds/spitz-japanese/beet-001.jpg"
+  );
+  const changeDogImage = () => {
+    setDogUrl("https://images.dog.ceo/breeds/akita/Akita_Inu_dog.jpg");
+  };
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src={dogUrl} alt="犬の画像"/>
+      <button onClick={changeDogImage}>更新</button>
     </div>
   )
 }
